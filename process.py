b688edbff30bd2e174845a47e126b19988627556
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-05-27 13:15:51 +0300

    Format with Black

--- process.py
+++ process.py
@@ -8,57 +8,53 @@ from gensim.models import TfidfModel
 
 
 def main():
-    #-------------------------- Text processing
-    with open('text.txt', 'r') as f:
+    # -------------------------- Text processing
+    with open("text.txt", "r") as f:
         raw = f.read()
 
-    print('\n----------------------\n\tRaw text\n----------------------')
+    print("\n----------------------\n\tRaw text\n----------------------")
     print(raw)
-
 
     sentences = list(get_sentences(raw))
     tokens = list([list(tokenize(s)) for s in sentences])
-    print('\n----------------------\n\tDictionary\n----------------------')
+    print("\n----------------------\n\tDictionary\n----------------------")
     d = Dictionary(tokens)
     print(d)
 
     t = tokens[2]
 
     print('\n----------------------\n\t"Bag of Words"\n----------------------')
-    print('{0} -> {1}'.format(t, d.doc2bow(t)))
+    print("{0} -> {1}".format(t, d.doc2bow(t)))
 
     corpus = list([d.doc2bow(t) for t in tokens])
     tfidf = TfidfModel(corpus)
 
-    print('\n----------------------\n\tTFIDF\n----------------------')
-    print('{0} -> {1}'.format(t, list(tfidf[corpus[2]])))
+    print("\n----------------------\n\tTFIDF\n----------------------")
+    print("{0} -> {1}".format(t, list(tfidf[corpus[2]])))
 
     results = [tfidf[c] for c in corpus]
 
-    #------------------------------ Training
+    # ------------------------------ Training
     # Normally, we would take this `y` data from csv file, where we have raw text and label together.
     # Cheating a bit, so we label anything with `Duke` inside as a label `1`
-    y = [1 if 'Duke' in t else 0 for t in tokens]
+    y = [1 if "Duke" in t else 0 for t in tokens]
     x = np.zeros((len(results), len(d)))
 
     for i, r in enumerate(results):
         for j, v in r:
             x[(i, j)] = v
 
-
     model = svm.LinearSVC()
     model.fit(x, y)
 
+    # ------------------------------ Testing
 
-    #------------------------------ Testing
+    print("\n----------------------\n\tPredicting labels\n----------------------")
 
-    print('\n----------------------\n\tPredicting labels\n----------------------')
-
-
-    s1 = 'Our Duke Vytautas'
-    s2 = 'Our King Vytautas'
-    s3 = 'You know nothing, John Snow'
-    s4 = 'Duke Nukem 3D'
+    s1 = "Our Duke Vytautas"
+    s2 = "Our King Vytautas"
+    s3 = "You know nothing, John Snow"
+    s4 = "Duke Nukem 3D"
 
     def prepare(s):
         return tfidf[d.doc2bow(tokenize(s))]
@@ -72,8 +68,8 @@ def main():
             x[(i, j)] = v
 
     for o, r in zip([s1, s2, s3, s4], model.predict(x)):
-        print('{0}: {1}'.format(o, r))
+        print("{0}: {1}".format(o, r))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
